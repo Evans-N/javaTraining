3f39e2a4832926dea9e8affe9ff5b999219e9493
Nathan Evans <nevans.dev@gmail.com>
2021-05-18 14:55:41 -0500

    adding in arrayLists, and String.

---
+++ src/com/qa/strings/stringExamples.java
@@ -0,0 +1,8 @@
+package com.qa.strings;
+
+public class stringExamples {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/qa/strings/stringManipulation.java
@@ -0,0 +1,8 @@
+package com.qa.strings;
+
+public class stringManipulation {
+	public static void main(String[] args) {
+		
+	}
+
+}
